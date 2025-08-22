b701b8903edeff032ebe6a4f92647bb9ce56597a
alexoh2bd <alexoh2020@gmail.com>
2025-08-22 13:07:21 -0400

    Cleaned up app, buttons and expanders

--- app.py
+++ app.py
@@ -10,30 +10,30 @@ url = f"https://api.openweathermap.org/data/2.5/weather"
 if "cities" not in st.session_state:
     st.session_state.cities = []
 st.title("Weather App PRO")
-city_input = st.text_input("Find City Weather", "Pyongyang")
-if st.button("Add Expander"):
-    if city_input: # Only add if the input is not empty
-        st.session_state.cities.append(city_input)
-        st.success("Expander added!")
-    else:
-        st.warning("Please enter some content.")
+city_input = st.text_input("Find City Weather", "Durham")
+if city_input: # Only add if the input is not empty
+    st.session_state.cities.append(city_input)
+else:
+    st.warning("Please enter some content.")
 
+st.header(f"City Weather")
 for idx, city in enumerate(st.session_state.cities):
-    with st.expander(f"Weather from {city}"):
-        params = {
+    params = {
             "q": city,
             "appid": API_KEY,
             "units": "metric"  # temperature in Celsius
         }
-        response = requests.get(url, params)
-        if response.status_code !=200:
-            st.write(F"{city} not found in API")
-        data = response.json()
-        for key, value in data['main'].items():
-            st.write(f"{key}: {value}")
-        # st.write(f"Temperature: {data['main']['temp']} degrees Celsius")
-        # st.write(f"Humidity: {data['main']['humidity']}%")
-        st.write(f"Description: {data['weather'][0]['description']}")
+    response = requests.get(url, params)
+    if response.status_code ==200:
+        with st.expander(f"{city}"):
+            data = response.json()
+            for key, value in data['main'].items():
+                st.write(f"{key}: {value}")
+            st.write(f"Description: {data['weather'][0]['description']}")
+    else:
+        st.session_state.cities.remove(city)
+        # st.write(F"{city} not found in API")
+
         
 # st.session_state.update(st.session_state.cities.append(city_input))
 # # for i, city in enumerate(st.session_state.cities):
